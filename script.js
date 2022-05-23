42039ae5739cf7f86736be463208383ef763d0f5
GVictorLO <gabrielvictor89@gmail.com>
2022-05-23 13:47:36 -0300

    resgantando o valor do input e criando a estrutura de validacao

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 console.log("Java script carregado no live server");
 
+function validaCPF(cpf) {
+  // for (var i in cpf) {
+  //   if (type(i) != number) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+  return true;
+}
+
 function validacao() {
   console.log("Validação iniciada");
+  var cpf = document.getElementById("cpf_digitado").value;
+  console.log(cpf);
+  var resultadoValidacao = validaCPF(cpf);
+  if (resultadoValidacao) {
+    document.getElementById("success").style.display = "block";
+  } else {
+    document.getElementById("error").style.display = "block";
+  }
 }
