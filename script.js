d076d01f4358af484e6e25cc8f232cc08aa7451d
GVictorLO <gabrielvictor89@gmail.com>
2022-05-23 14:27:02 -0300

    implementacao do algoritimo para verificar digitos do cpf

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 console.log("Java script carregado no live server");
 
 function validaCPF(cpf) {
-  // for (var i in cpf) {
-  //   if (type(i) != number) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
-  return true;
+  if (cpf.length != 11) {
+    return false;
+  } else {
+    var numeros = cpf.substring(0, 9);
+    var digitos = cpf.substring(9);
+
+    var soma = 0;
+    for (var i = 10; i > 1; i--) {
+      soma += numeros.charAt(10 - i) * i;
+    }
+
+    // validação do primeiro dígito
+    var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(0)) {
+      return false;
+    }
+
+    soma = 0;
+    numeros = cpf.substring(0, 10);
+    for (var k = 11; k > 1; k--) {
+      soma += numeros.charAt(11 - k) * k;
+    }
+    // validação do segundo do primeiro dígito
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(1)) {
+      return false;
+    }
+
+    return true;
+  }
 }
 
 function validacao() {
